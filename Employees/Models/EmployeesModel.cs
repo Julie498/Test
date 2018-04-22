dddcb0308a7bd89f2acda5053286719c5d19e123
Julie498 <nhanho498@gmail.com>
2018-04-23 00:19:24 +0700

    first commit

---
+++ Employees/Models/EmployeesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employees.Models
+{
+    public class EmployeesModel
+    {
+        public string Id_Employee { set; get; }
+        public string Fullname { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+}
